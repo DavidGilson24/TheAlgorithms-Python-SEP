889f6c90874cd56fe9258096721a484124c95efa
David Gilson <cs@david-gilson.com>
2024-06-20 14:25:03 +0200

    Update insertion_sort.py

--- sorts/insertion_sort.py
+++ sorts/insertion_sort.py
@@ -38,10 +38,7 @@ def insertion_sort(collection):
 
 
 if __name__ == '__main__':
-    try:
-        raw_input          # Python 2
-    except NameError:
-        raw_input = input  # Python 3
+    raw_input = input  # Python 3
 
     user_input = raw_input('Enter numbers separated by a comma:\n').strip()
     unsorted = [int(item) for item in user_input.split(',')]
